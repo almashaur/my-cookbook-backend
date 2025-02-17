b6a5471d2d7d69161dc2e329858852fc1f53a5a2
Ahmed Almashaur <ahmedalmashaur@gmail.com>
2025-02-17 17:36:14 +0300

    Update and delete functionality completed

--- controllers/users.js
+++ controllers/users.js
@@ -26,7 +26,43 @@ router.get("/:userId", verifyToken, async (req, res) => {
 });
 
 // Update a user by id (Protected route)
+router.put("/:userId", verifyToken, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      return res.status(403).json({ err: "Unauthorized" });
+    }
 
-// Delete a user by id (Protected route)
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ err: "User not found." });
+    }
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// Delete a user by ID (Protected route)
+router.delete("/:userId", verifyToken, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      return res.status(403).json({ err: "Unauthorized" });
+    }
+
+    const user = await User.findByIdAndDelete(req.params.userId);
+
+    if (!user) {
+      return res.status(404).json({ err: "User not found." });
+    }
+
+    res.json({ msg: "User deleted." });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 module.exports = router;
